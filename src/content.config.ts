c898eb37600bf6d49b7a3987a0837156c59883a7
riofauzi708 <riofauzi708@gmail.com>
2025-09-18 15:58:40 +0700

    change blog to docs

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,32 +1,37 @@
 import { defineCollection, z } from 'astro:content';
 
-const blog = defineCollection({
+// === Docs ===
+const docs = defineCollection({
   schema: z.object({
-    title: z.string(),
-    description: z.string(),
-    pubDate: z.coerce.date(),
-    updatedDate: z.coerce.date().optional(),
-    heroImage: z.string().optional(),
-    body: z.string().optional(),
+    title: z.string(),                                
+    description: z.string(),                          
+    pubDate: z.coerce.date(),                          
+    updatedDate: z.coerce.date().optional(),           
+    heroImage: z.string().optional(),                  
+    tags: z.array(z.string()).optional(),              
+    draft: z.boolean().default(false).optional(),      
+    body: z.string().optional(),                       
   }),
 });
 
+// === Projects ===
 const projects = defineCollection({
   schema: z.object({
-    title: z.string(),
-    description: z.string(),
-    heroImage: z.string(),
-    secondImage: z.string().optional(),
-    body: z.string().optional(),
-	pubDate: z.coerce.date().optional(),
-	sourceUrl: z.string().optional(),
-  projectType: z.enum(["Personal", "Team"]).optional(),
+    title: z.string(),                                 
+    description: z.string(),                           
+    pubDate: z.coerce.date().optional(),               
+    heroImage: z.string().optional(),                  
+    projectType: z.enum(["Personal", "Team"]).default("Personal").optional(),
+    platform: z.enum(["Mobile", "Website", "Other"]).default("Website").optional(),
     techStack: z.array(z.string()).optional(),
-    liveUrl: z.string().optional(),
+    liveUrl: z.string().url().optional(),
+    sourceUrl: z.string().url().optional(),
+    draft: z.boolean().default(false).optional(),
+    body: z.string().optional(),
   }),
 });
 
 export const collections = {
-  blog,
+  docs,
   projects,
 };
